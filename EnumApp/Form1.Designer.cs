d3d9bea07c2d7877175bb9a080ce6653ff827d3b
Elizabethpv <48952600+Elizabethpv@users.noreply.github.com>
2021-03-23 21:01:14 +0530

    Add project files.

---
+++ EnumApp/Form1.Designer.cs
@@ -0,0 +1,75 @@
+﻿
+namespace EnumApp
+{
+    partial class enumSwitch
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.cmbSwitchBox = new System.Windows.Forms.ComboBox();
+            this.btnSwitch = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // cmbSwitchBox
+            // 
+            this.cmbSwitchBox.FormattingEnabled = true;
+            this.cmbSwitchBox.Items.AddRange(new object[] {
+            "ON",
+            "OFF"});
+            this.cmbSwitchBox.Location = new System.Drawing.Point(268, 171);
+            this.cmbSwitchBox.Name = "cmbSwitchBox";
+            this.cmbSwitchBox.Size = new System.Drawing.Size(201, 21);
+            this.cmbSwitchBox.TabIndex = 0;
+            // 
+            // btnSwitch
+            // 
+            this.btnSwitch.Location = new System.Drawing.Point(268, 231);
+            this.btnSwitch.Name = "btnSwitch";
+            this.btnSwitch.Size = new System.Drawing.Size(201, 34);
+            this.btnSwitch.TabIndex = 1;
+            this.btnSwitch.Text = "Switch";
+            this.btnSwitch.UseVisualStyleBackColor = true;
+            this.btnSwitch.Click += new System.EventHandler(this.btnSwitch_Click);
+            // 
+            // enumSwitch
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btnSwitch);
+            this.Controls.Add(this.cmbSwitchBox);
+            this.Name = "enumSwitch";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ComboBox cmbSwitchBox;
+        private System.Windows.Forms.Button btnSwitch;
+    }
+}
+
---
+++ EnumApp/Form1.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace EnumApp
+{
+    public partial class enumSwitch : Form
+    {
+        public enumSwitch()
+        {
+            InitializeComponent();
+        }
+        public SwitchResult Tossing(Switch switchs)
+        {
+            int temp;
+            Random random= new Random();
+            temp = random.Next(0, 2);
+
+            if (temp == (int)switchs)
+            {
+                return SwitchResult.Light;
+            }
+            else
+            {
+                return SwitchResult.Dark;
+            }
+        }
+        private void btnSwitch_Click(object sender, EventArgs e)
+        {
+            SwitchResult result;
+
+            if (cmbSwitchBox.Text == "ON")
+            {
+                result = Tossing(Switch.ON);
+                this.Text = result.ToString();
+               
+               
+            }
+            else if (cmbSwitchBox.Text == "OFF")
+            {
+                result = Tossing(Switch.OFF);
+                this.Text = result.ToString();
+               
+            }
+            else
+            {
+                MessageBox.Show("Please On Switch");
+            }
+
+            if(this.Text=="Light")
+            {
+                this.BackColor = Color.White;
+            }
+            else
+            {
+                this.BackColor = Color.Black;
+
+            }
+        }
+    }
+}
